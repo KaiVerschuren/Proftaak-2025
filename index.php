2cdcec006d740a24e8d047b2bfaeddafe07e9705
KaiVerschuren <144013440+KaiVerschuren@users.noreply.github.com>
2025-05-27 20:36:45 +0200

    done some minor changes
    classnames etc.

--- index.php
+++ index.php
@@ -14,27 +14,44 @@ headerFunc();
                 Welcome to <span class="titleSecondary">GoodieMaticAa</span>
             </h1>
             <p class="heroParagraph">Easily order snacks from our school vending machine,<br /> right from this website!</p>
-            <button class="btn">Order now!</button>
+            <button class="btnSecondary">Order now!</button>
         </div>
         <div>
             <img src="./assets/vendingMachineIllustration.png" alt="Vending machine illustration" class="heroImg">
         </div>
     </div>
     <div class="top">
-        <div class="topTitle">
-            <h2>Our top bought products</h2>
+        <div>
+            <h1 class="topTitle">Our <span class="titlePrimary">top bought</span> products</h2>
         </div>
         <div class="topCardWrapper">
             <div class="topCard cardShadow">
-                <img src="./assets/placeholderCards.png" alt="Placeholder" class="topCardImg">
+                <img src="./assets/placeholderCards.png" class="topCardImg">
                 <div class="topCardInfo">
-                    <h2 class="topCartTitle">Product one</h2>
-                    <p>Description</p>
-                    <div>
-                        <button class="btnPrimary topCartButton">Order now!</button>
-                    </div>
+                    <h2 class="topCardTitle">SmoothWrite Pen</h2>
+                    <p class="topCartText">Reliable ballpoint pen with smooth ink flow. Perfect for daily notes or doodles.</p>
+                    <button class="btnPrimary topCardButton">Order now!</button>
                 </div>
             </div>
+
+            <div class="topCard cardShadow">
+                <img src="./assets/placeholderCards.png" alt="3D Printed AA Statue" class="topCardImg">
+                <div class="topCardInfo">
+                    <h2 class="topCardTitle">AA 3D Print</h2>
+                    <p class="topCartText">Colored 3D-printed "AA" statue. Great for desk decoration or branding props.</p>
+                    <button class="btnPrimary topCardButton">Order now!</button>
+                </div>
+            </div>
+
+            <div class="topCard cardShadow">
+                <img src="./assets/placeholderCards.png" alt="White Monster Can" class="topCardImg">
+                <div class="topCardInfo">
+                    <h2 class="topCardTitle">Monster Ultra</h2>
+                    <p class="topCartText">Crisp white Monster energy drink. Zero sugar, full energy. Stay sharp!</p>
+                    <button class="btnPrimary topCardButton">Order now!</button>
+                </div>
+            </div>
+
         </div>
     </div>
-</main>  
+</main>
