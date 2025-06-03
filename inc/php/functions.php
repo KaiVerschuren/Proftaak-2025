e0ce390560a19e3b78ec7e0ac3279f3d77f87598
KaiVerschuren <91733@roc-teraa.nl>
2025-06-03 13:49:43 +0200

    authentication without database

--- inc/php/functions.php
+++ inc/php/functions.php
@@ -2,13 +2,7 @@
 
 function head($page)
 {
-<<<<<<< Updated upstream
 ?>
-=======
-    global $currentPage;
-    $currentPage = $page;
-    ?>
->>>>>>> Stashed changes
     <!DOCTYPE html>
     <html lang="en">
 
@@ -32,32 +26,6 @@ function head($page)
 
 function headerFunc()
 {
-<<<<<<< Updated upstream
-?>
-    <header class="container">
-        <div class="headerTitle titlePrimary">
-            <h1>GoodieMaticAa</h1>
-        </div>
-        <nav>
-            <ul class="noStyleUL headerLinks">
-                <li><a href="index.php" class="headerLink">Home</a></li>
-                <li><a href="about.php" class="headerLink">About</a></li>
-                <li><a href="contact.php" class="headerLink">Contact</a></li>
-            </ul>
-        </nav>
-        <div class="headerCallToAction">
-            <button class="btn">
-                Order 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="btnIcon">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                </svg>
-            </button>
-        </div>
-    </header>
-<?php
-}
-
-=======
     global $currentPage;
     ?>
 
@@ -102,5 +70,4 @@ function footerFunc()
     <?php
 }
 
->>>>>>> Stashed changes
 ?>
